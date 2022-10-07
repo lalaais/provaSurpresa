5abfc5207f27406e8f3ef74ec2b32c82e51369f3
lalais <lais.s.almeidas@gmail.com>
2022-10-07 13:03:37 -0300

    imagens e codigo

--- src/pages/acai/index.js
+++ src/pages/acai/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import '/.index.sass';
 
 export default function Index() {
@@ -9,3 +10,25 @@ export default function Index() {
     )
 }
 
+export default function Acai () {
+    const [qtdPeq, setQtdPeq] = useState(0)
+    const [qtdMed, setQtdMed] = useState(0)
+    const [qtdGra, setQtdGra] = useState(0)
+
+    const [desc, setDesc] = useState(0)
+    const [resul, setResul] = useState(0)
+
+    function Calcular() {
+        let total = qtdPeq * 13.50 + qtdMed * 15 + qtdGra * 17.50;
+        let desconto = total * desc / 100
+
+        let final = total - desconto 
+        setResul(final)
+    }
+
+    useEffect(() => {
+        Calcular()
+    }, [qtdPeq, qtdMed, qtdGra, desc])
+}
+
+[]
